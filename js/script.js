4e68e5f716009c06650335d3608736913c53f4fc
dude0195 <roman.kos.0195@gmail.com>
2021-10-05 17:54:48 +0700

    created section reviews

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.review-slider', {
+  loop: false,
+  slidesPerView: 'auto',
+  //   centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoHeight: true,
+});
+// const swiper = new Swiper('.review-slider', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
